707414ea699d37b39d21a7ba1661a6164959b06d
tr13xxt <166804472+tr13xxt@users.noreply.github.com>
2024-05-21 00:34:56 +0500

    Update script.js
    hz

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 let cnt1=document.getElementById('cnt1');
-function like1(){
-    if(cnt1.textContent=='0'){
-        cnt1.textContent='1';
-    }else{
-        cnt1.textContent='0';
-    }
+const like1 = () => {
+    cnt1.textContent = '1';
 }
